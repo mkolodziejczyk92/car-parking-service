6def37a8659e6f0eb927ec97dbfc0b71161164d6
mkolodziejczyk92 <marcin.kdz92@gmail.com>
2022-12-04 14:29:17 +0100

    Add initial commit.

---
+++ src/main/java/io/mkolodziejczyk92/carparkingservice/CarParkingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.mkolodziejczyk92.carparkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarParkingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mkolodziejczyk92/carparkingservice/CarParkingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mkolodziejczyk92.carparkingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
