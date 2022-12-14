de5f5cc8c573f6c41ee4d4db492919bce7379d09
mkolodziejczyk92 <marcin.kdz92@gmail.com>
2022-12-14 05:47:50 +0100

    Add User and Admin controller

--- src/main/java/io/mkolodziejczyk92/carparkingservice/api/AdminController.java
+++ src/main/java/io/mkolodziejczyk92/carparkingservice/api/AdminController.java
@@ -1,12 +1,11 @@
 package io.mkolodziejczyk92.carparkingservice.api;
 
 import io.mkolodziejczyk92.carparkingservice.api.dto.CarParkIdDto;
+import io.mkolodziejczyk92.carparkingservice.domain.CarPark;
 import io.mkolodziejczyk92.carparkingservice.domain.CarParkId;
 import io.mkolodziejczyk92.carparkingservice.domain.CarParkService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.UUID;
@@ -28,8 +27,19 @@ public class AdminController {
                 + carParkId.rawValue())).body(new CarParkIdDto(carParkId.rawValue()));
     }
 
+    @PatchMapping(value = "/car-park/{carParkId}")
+    public ResponseEntity<CarPark> updateParkingValues(@PathVariable String carParkId,
+                                                       @RequestParam String carParkName){
+        CarParkId carParkIdIn = new CarParkId(carParkId);
+        return ResponseEntity.ok().body(carParkService.updateCarPark(carParkIdIn, carParkName));
+    }
 
-
+    @DeleteMapping(value = "/car-park/{carParkId}")
+    public ResponseEntity<CarPark> deleteCarParkValue(@PathVariable String carParkId){
+        CarParkId carParkIdIn = new CarParkId(carParkId);
+        carParkService.deleteCarPark(carParkIdIn);
+        return ResponseEntity.noContent().build();
+    }
 
 
 
